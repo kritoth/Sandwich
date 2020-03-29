03ae968eb91f8b6a922bf77719fe19fc5d5a18c3
kritoth <kritoth@gmail.com>
2020-03-29 12:43:06 +0200

    Create JSON parsing in JsonUtils

--- app/src/main/java/com/udacity/sandwichclub/utils/JsonUtils.java
+++ app/src/main/java/com/udacity/sandwichclub/utils/JsonUtils.java
@@ -12,6 +12,35 @@ import java.util.List;
 public class JsonUtils {
 
     public static Sandwich parseSandwichJson(String json) {
-        return null;
+        String mainName = "";
+        List<String> alsoKnownAs = new ArrayList<String>();
+        String placeOfOrigin = "";
+        String description = "";
+        String image = "";
+        List<String> ingredients = new ArrayList<String>();
+
+        JSONObject sandwichObj = null;
+        try {
+            sandwichObj = new JSONObject(json);
+            JSONObject name = sandwichObj.getJSONObject("name");
+            mainName = name.getString("mainName");
+
+            JSONArray akas = name.getJSONArray("alsoKnownAs");
+            for(int i=0; i<akas.length(); i++){
+                alsoKnownAs.add((String)akas.get(i));
+            }
+            placeOfOrigin = sandwichObj.getString("placeOfOrigin");
+            description = sandwichObj.getString("description");
+            image = sandwichObj.getString("image");
+
+            JSONArray ingredientsArray = name.getJSONArray("ingredients");
+            for(int i=0; i<ingredientsArray.length(); i++){
+                ingredients.add((String)ingredientsArray.get(i));
+            }
+        } catch (JSONException e) {
+            e.printStackTrace();
+            return new Sandwich();
+        }
+        return new Sandwich(mainName, alsoKnownAs, placeOfOrigin, description, image, ingredients);
     }
 }
